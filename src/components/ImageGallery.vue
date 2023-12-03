416315856875ae0a77bc448b8fb1fddc5f985f7d
Surya Bumantara <bumantara.surya2213@gmail.com>
2023-12-03 20:58:44 +0700

    tampilan productDetail

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-carousel
+    :hide-delimiters="true"
+    v-model="carousel"
+    height="420"
+    class="rounded-lg mb-2"
+    @update:model-value="checkPage"
+  >
+    <v-carousel-item
+      v-for="(item,i) in images"
+      :key="i"
+      :src="item.path"
+    >
+      <div class="d-flex">
+        <v-spacer />
+        <v-chip
+          v-if="item.variant"
+          class="ma-2"
+          label
+          variant="flat"
+        >
+          {{ item.variant.name }}
+        </v-chip>
+      </div>
+    </v-carousel-item>
+  </v-carousel>
+  <v-data-iterator
+    :items="images"
+    :items-per-page="itemPerPage"
+    v-model:page="page"
+  >
+    <template #default="{ items, page, prevPage, pageCount, nextPage, itemsPerPage}">
+      <div class="d-flex align-center justify-space-between">
+        <v-btn
+          :disabled="page === 1"
+          icon="mdi-chevron-left"
+          density="compact"
+          variant="tonal"
+          rounded
+          @click="prevPage"
+        />
+        <v-spacer />
+        <v-card
+          v-for="(item, i) in items"
+          :key="item.raw"
+          :variant="((page - 1) * itemsPerPage) + i === carousel ? 'outlined' : 'flat'"
+          class="mx-1"
+          @click="carousel = ((page - 1) * itemsPerPage) + i"
+        >
+          <v-img
+            cover
+            width="60"
+            aspect-ratio="1"
+            :src="item.raw.path"
+          />
+        </v-card>
+        <v-spacer />
+        <v-btn
+          :disabled="page >= pageCount"
+          icon="mdi-chevron-right"
+          density="compact"
+          variant="tonal"
+          rounded
+          @click="nextPage"
+        />
+      </div>
+    </template>
+  </v-data-iterator>
+</template>
+<script lang="ts">
+import { ProductImages } from '@utils/types'
+import { ref } from 'vue'
+export default {
+	props: {
+		images: {
+			type: Array as () => ProductImages[],
+			default: () => []
+		}
+	},
+	setup() {
+		const carousel = ref(0)
+		const itemPerPage = ref(5)
+		const page = ref(1)
+
+		const checkPage = (val: number) => {
+			console.log((itemPerPage.value * page.value) - 1)
+			if (val > ((itemPerPage.value * page.value) - 1)) {
+				page.value += 1
+			} else if (val < ((itemPerPage.value * (page.value - 1)))) {
+				page.value -= 1
+			}
+		}
+		
+		return {
+			carousel,
+			itemPerPage,
+			page,
+      
+			checkPage
+		}
+	}
+}
+</script>
--- src/views/Product/ProductDetail.vue
+++ src/views/Product/ProductDetail.vue
@@ -8,23 +8,137 @@
     >
       <v-col
         cols="12"
-        md="6"
+        md="5"
       >
-        <v-carousel hide-delimiters>
-          <v-carousel-item
-            v-for="(item,i) in carouselItem"
-            :key="i"
-            :src="item"
-          />
-        </v-carousel>
+        <image-gallery
+          :images="product.images"
+        />
       </v-col>
       <v-col
         cols="12"
-        md="6"
+        md="7"
       >
-        <v-card>
-          {{ product.name }}
-        </v-card>
+        <template v-if="product && product.variants">
+          <v-card
+            flat
+          >
+            <v-card-title>
+              {{ product.name }}
+            </v-card-title>
+            <v-card-text>
+              <v-chip-group
+                v-if="!product.single_variant"
+                class="my-2"
+                v-model="selectedVariant"
+                @update:model-value="checkAmount"
+              >
+                <v-chip
+                  v-for="(item, i) in product.variants"
+                  :key="i"
+                  label
+                  variant="outlined"
+                >
+                  {{ item.name }}
+                </v-chip>
+              </v-chip-group>
+              <p class="text-h6 mb-3 font-weight-light">
+                {{ formatCurrency(product.variants[selectedVariant].price) }}
+              </p>
+              <p class="text-subtitle-2 mb-1">
+                Stok: {{ product.variants[selectedVariant].stock }}
+              </p>
+              <v-responsive
+                max-width="128px"
+                class="mb-8"
+              >
+                <v-text-field
+                  variant="outlined"
+                  type="number"
+                  hide-details="auto"
+                  hide-spin-buttons
+                  density="compact"
+                  v-model="amount"
+                  class="centered-input"
+                  @update:model-value="checkAmount"
+                >
+                  <template #prepend-inner>
+                    <v-btn
+                      icon
+                      density="compact"
+                      variant="text"
+                      @click="reduceAmount"
+                    >
+                      <v-icon>mdi-minus</v-icon>
+                    </v-btn>
+                  </template>
+                  <template #append-inner>
+                    <v-btn
+                      icon
+                      density="compact"
+                      variant="text"
+                      @click="increaseAmount"
+                    >
+                      <v-icon>mdi-plus</v-icon>
+                    </v-btn>
+                  </template>
+                </v-text-field>
+              </v-responsive>
+              <v-row class="mb-6">
+                <template v-if="isLoggedIn">
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    class="py-1"
+                  >
+                    <v-btn
+                      block
+                      color="primary"
+                      prepend-icon="mdi-cash"
+                      flat
+                    >
+                      Beli sekarang
+                    </v-btn>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    class="py-1"
+                  >
+                    <v-btn
+                      block
+                      color="black"
+                      prepend-icon="mdi-cart"
+                      flat
+                    >
+                      Masukkan keranjang
+                    </v-btn>
+                  </v-col>
+                </template>
+                <template v-else>
+                  <v-col
+                    cols="12"
+                    class="py-1"
+                  >
+                    <v-btn
+                      block
+                      color="black"
+                      prepend-icon="mdi-cash"
+                      flat
+                      @click="() => $router.push({ name: 'login' })"
+                    >
+                      Beli sekarang
+                    </v-btn>
+                  </v-col>
+                </template>
+              </v-row>
+              <v-divider class="mb-6" />
+              <p
+                v-html="product.description"
+                class="mb-8"
+              />
+            </v-card-text>
+          </v-card>
+        </template>
       </v-col>
     </v-row>
   </XyzTransition>
@@ -33,32 +147,65 @@
 import { onMounted, ref } from 'vue'
 import useProduct from '@composables/useProduct'
 import { useRoute } from 'vue-router'
+import ImageGallery from '@components/ImageGallery.vue'
+import { formatCurrency } from '@utils/filter'
+import { useAuthStore } from '@/store/modules'
 
 export default {
+	components: {
+		ImageGallery
+	},
 	setup() {
 		const route = useRoute()
+		const { isLoggedIn } = useAuthStore()
 		const { getProductDetail, loadingProductDetail, product } = useProduct()
+		const amount = ref(1)
 
 		onMounted(() => {
 			const id = +(Array.isArray(route.params.id) ? route.params.id[0] : route.params.id)
 			getProductDetail(id)
 		})
 
-		const carouselItem = ref([
-			new URL('@assets/carousel/carousel-1.webp', import.meta.url).href,
-			new URL('@assets/carousel/carousel-2.webp', import.meta.url).href,
-			new URL('@assets/carousel/carousel-3.webp', import.meta.url).href,
-			new URL('@assets/carousel/carousel-4.webp', import.meta.url).href,
-			new URL('@assets/carousel/carousel-5.webp', import.meta.url).href,
-			new URL('@assets/carousel/carousel-6.webp', import.meta.url).href,
-			new URL('@assets/carousel/carousel-7.webp', import.meta.url).href,
-		])
+		const reduceAmount = () => {
+			if (amount.value > 1) {
+				amount.value -= 1
+			}
+		}
+
+		const increaseAmount = () => {
+			if (amount.value < product.value.variants[selectedVariant.value].stock) {
+				amount.value += 1
+			}
+		}
+
+		const checkAmount = () => {
+			if (amount.value < 1) {
+				amount.value = 1
+			} else if (amount.value > product.value.variants[selectedVariant.value].stock) {
+				amount.value = product.value.variants[selectedVariant.value].stock
+			}
+		}
+    
+		const selectedVariant = ref(0)
     
 		return {
-			carouselItem,
 			loadingProductDetail,
-			product
+			product,
+			selectedVariant,
+			amount,
+
+			formatCurrency,
+			reduceAmount,
+			increaseAmount,
+			checkAmount,
+
+			isLoggedIn
 		}
 	},
 }
 </script>
+<style scoped>
+    .centered-input >>> input {
+      text-align: center
+    }
+</style>
